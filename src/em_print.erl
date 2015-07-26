c3f550d94e651a0b7bd9a359f8c017ad55678f12
Maxim Velesyuk <loz.accs@gmail.com>
2015-07-26 20:47:02 +0300

    Fixed string printing.

--- src/em_print.erl
+++ src/em_print.erl
@@ -19,7 +19,7 @@ format_object1([H|T], Env) ->
   ["(", Fh, " . ", Ft, ")"];
 format_object1([], _Env) -> "nil";
 format_object1({symbol, S}, _Env) -> S;
-format_object1({string, S}, _Env) -> S;
+format_object1({string, S}, _Env) -> ["\"", S, "\""];
 format_object1(I, _Env) when is_integer(I) -> integer_to_list(I);
 format_object1(F, _Env) when is_float(F) -> float_to_list(F);
 format_object1(O, _Env) -> io_lib:format("#<~p>", [O]).
